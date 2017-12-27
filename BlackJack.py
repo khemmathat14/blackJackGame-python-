a9f9af3c2ed12ab5b84a6601212f347057daa358
Unfrez141 <Khemathat@khemmathats-MacBook-Pro.local>
2017-12-27 22:52:05 +0700

    Add function check A

--- BlackJack.py
+++ BlackJack.py
@@ -1,21 +1,24 @@
 import random
 class player(object):
+
     def __init__(self,bankroll=100):
         self.bankroll = bankroll
+        self.score = 0
     
     def add_bankroll(self,amout):
         self.bankroll += amout
 
     
     def minus_bankroll(self,amout):
-        self.bankroll -= amout
+        if self.bankroll - amout >0:
+            self.bankroll -= amout
+        else:
+            print("You No Money to Bet")
 
+    def scorePlayer(self,score21=0):
+        self.score += score21
+        return self.score
 
-sam = player(1000)
-sam.bankroll
-sam.add_bankroll(123)
-sam.minus_bankroll(123)
-print(sam.bankroll)
 
 
     
@@ -36,16 +39,41 @@ class setdeck(object):
         random.shuffle(self.lst)
 
     #Card use
-    def drawDeck(self,first=1):
+    def drawDeck(self):
         print(self.lst)
-        for x in range(1,first+1):
-            pop_item = self.lst.pop()
+        pop_item = self.lst.pop()
+        print(pop_item)
+        if pop_item == "K" or pop_item == "Q" or pop_item == "J":
+            pop_item = 10
             print(pop_item)
-        print(self.lst)
+        return pop_item
+
+
+
+amout = input("Please enter amout to bet : ")
+sam = player(100)
+sam.bankroll
+sam.minus_bankroll(amout)
+print(sam.bankroll)
+
 
 
 num = input("Enter volum deck: ")
 deck1 = setdeck(num)
 deck1.createDeck()
 deck1.shuffDeck()
-deck1.drawDeck(1)
+
+def checkA(ch):
+    print(type(sam.score))
+    if ch == "A" and int(sam.score) > 11:
+        sam.scorePlayer(1)
+    elif ch == "A" and int(sam.score) <12:
+        sam.scorePlayer(10)
+    else:
+        sam.scorePlayer(ch)
+    
+    return sam.score
+
+checkA(deck1.drawDeck())
+checkA(deck1.drawDeck())
+print(sam.score)
